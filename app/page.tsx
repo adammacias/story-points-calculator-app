d0b2798231c18640c0a2ef1cd0169c91fab9d155
adammacias <1139893+adammacias@users.noreply.github.com>
2023-05-02 13:29:31 -0300

    feat(app): create MVP homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,170 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useState } from 'react';
+import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
+import { Slider, SliderSingleProps } from 'antd';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+const MESSAGES = {
+  uncertainty: ' Incerteza',
+  complexity: 'Complexidade',
+  points: new Map([
+    [1, { icon: '🛴', title: '1 Ponto' }],
+    [2, { icon: '🚲', title: '2 Pontos' }],
+    [3, { icon: '🛵', title: '3 Pontos' }],
+    [5, { icon: '🚗 ', title: '5 Pontos' }],
+    [8, { icon: '🚛', title: '8 Pontos' }],
+    [13, { icon: '🚤', title: '13 Pontos' }],
+    [20, { icon: '🚁', title: '20 Pontos' }],
+    [40, { icon: '🛩', title: '40 Pontos' }],
+    [100, { icon: '🚀', title: '100 Pontos' }],
+  ]),
+  description: new Map([
+    ['complexityHigh', 'Complexidade e o esforço para entregar está alta, que tal DIVIDIR a tarefa em partes menores?'],
+    ['uncertaintyHigh', 'Incerteza técnica e/ou de negócio está alta, que tal REFINAR a tarefa?'],
+    ['ok', 'Tudo certo! Tarefa com tamanho ideal.']
+  ])
 }
+
+const RANGE_OPTIONS = [
+  { value: 1, label: 'Mínima' },
+  { value: 2, label: 'Pouca' },
+  { value: 3, label: 'Moderada' },
+  { value: 4, label: 'Extrema' },
+  { value: 5, label: 'Máxima' }
+]
+
+const MATRIX = [
+  [1, 2, 3, 5, 8],
+  [2, 3, 5, 8, 13],
+  [3, 5, 8, 13, 20],
+  [5, 8, 13, 20, 40],
+  [8, 13, 20, 40, 100],
+];
+
+const LIMIT_POINTS_TO_UNCERTAINTY = 3
+
+const LIMIT_POINTS_TO_COMPLEXITY = 3
+
+type FormProps = {};
+
+type FormData = {
+  uncertainty: number;
+  complexity: number;
+};
+
+const IconSlider: React.FC<SliderSingleProps> = (props) => {
+  const { max, min, value } = props;
+  const mid = Number((((max || 0) - (min || 0)) / 2).toFixed(5));
+  const preColorCls = (value || 0) > mid ? '' : 'icon-wrapper-active';
+  const nextColorCls = (value || 0) > mid ? 'icon-wrapper-active' : '';
+
+  return (
+    <div className="icon-wrapper">
+      <SmileOutlined className={preColorCls} />
+      <Slider {...props} />
+      <FrownOutlined className={nextColorCls} />
+    </div>
+  );
+};
+
+const Home: React.FC<FormProps> = () => {
+  const [formData, setFormData] = useState<FormData>({
+    uncertainty: 0,
+    complexity: 0,
+  });
+
+  const handleInputChange = (
+    value: number,
+    field: keyof FormData
+  ) => {
+    setFormData({
+      ...formData,
+      [field]: value,
+    });
+  };
+
+  const result = MATRIX[formData.uncertainty][formData.complexity];
+
+  const formatter = (value: any) => RANGE_OPTIONS[value].label;
+
+  const description = () => {
+    if (formData.complexity >= LIMIT_POINTS_TO_COMPLEXITY) {
+      return MESSAGES.description.get('complexityHigh')
+    }
+
+    if (formData.uncertainty >= LIMIT_POINTS_TO_UNCERTAINTY) {
+      return MESSAGES.description.get('uncertaintyHigh')
+    }
+
+    return MESSAGES.description.get('ok')
+  }
+
+  const contextBackgroundColorClass = () => {
+    if (formData.complexity >= LIMIT_POINTS_TO_COMPLEXITY) {
+      return 'bg-red-200'
+    }
+
+    if (formData.uncertainty >= LIMIT_POINTS_TO_UNCERTAINTY) {
+      return 'bg-yellow-200'
+    }
+
+    return 'bg-green-200'
+  }
+
+  return (
+    <main className={`min-h-screen text-center bg-white ${inter.className}`}>
+      {
+        MESSAGES.points.get(result) &&
+        <section
+          className={`grid content-center ${contextBackgroundColorClass()}`}
+          style={{ gridArea: 'result' }}
+        >
+          <p className="leading-normal text-8xl">
+            {MESSAGES.points.get(result)?.icon}
+          </p>
+          <p className="text-5xl font-bold leading-normal">
+            {MESSAGES.points.get(result)?.title}
+          </p>
+          <p className="max-w-2xl mx-auto text-3xl leading-snug line">
+            {description()}
+          </p>
+        </section>
+      }
+
+      <form
+        className='grid content-center gap-12 justify-self-center'
+        style={{ gridArea: 'form', minWidth: '800px' }
+        }
+      >
+        <fieldset>
+          <p className="mb-4 text-2xl">
+            {MESSAGES.uncertainty}
+          </p>
+          <IconSlider
+            min={0} max={4}
+            value={formData.uncertainty}
+            tooltip={{ formatter }}
+            onChange={value => handleInputChange(value, 'uncertainty')}
+          />
+        </fieldset>
+
+        <fieldset>
+          <p className="mb-4 text-2xl">
+            {MESSAGES.complexity}
+          </p>
+          <IconSlider
+            min={0} max={4}
+            value={formData.complexity}
+            tooltip={{ formatter }}
+            onChange={value => handleInputChange(value, 'complexity')}
+          />
+        </fieldset>
+      </form>
+    </main>
+  );
+};
+
+export default Home;
